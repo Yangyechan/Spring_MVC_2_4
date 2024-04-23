06bbbb8cae4e2f3015595682a79d6bdaeed98241
yangyechan <yac392045@mokpo.ac.kr>
2024-04-23 17:22:43 +0900

    "lecture_5 Form 전송 객체 분리"

--- src/main/java/hello/itemservice/domain/item/Item.java
+++ src/main/java/hello/itemservice/domain/item/Item.java
@@ -11,17 +11,18 @@ import javax.validation.constraints.NotNull;
 @Data
 public class Item {
 
+//    @NotNull(groups = UpdateCheck.class) //수정 요구사항 추가
     private Long id;
 
-    @NotBlank
+//    @NotBlank(groups = {UpdateCheck.class, SaveCheck.class})
     private String itemName;
 
-    @NotNull
-    @Range(min = 1000, max = 1000000)
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Range(min = 1000, max = 1000000, groups = {UpdateCheck.class, SaveCheck.class})
     private Integer price;
 
-    @NotNull
-    @Max(9999)
+//    @NotNull(groups = {UpdateCheck.class, SaveCheck.class})
+//    @Max(value = 9999, groups = SaveCheck.class) //수정 요구사항 추가
     private Integer quantity;
 
     public Item() {
